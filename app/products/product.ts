aae7b385c7a0d82058220bc7c173ee5b3cc3422f
VishalTav <vishal.jagani@tavant.com>
2017-05-16 15:28:08 +0530

    Add files via upload

---
+++ app/products/product.ts
@@ -0,0 +1,12 @@
+/* Defines the product entity */
+export interface IProduct {
+    id: number;
+    employeeName: string;
+    employeeCode: string;
+    releaseDate: string;
+    Salary: number;
+    description: string;
+    starRating: number;
+    imageUrl: string;
+}
+
